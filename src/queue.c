1ce4008d2099f1fedd75c779e9af93ea8f788004
manhattien2353178 <tienmanhat11a321@gmail.com>
2025-04-04 09:15:03 +0700

    test contribution queue.c

--- src/queue.c
+++ src/queue.c
@@ -12,6 +12,11 @@ int empty(struct queue_t *q)
 void enqueue(struct queue_t *q, struct pcb_t *proc)
 {
     /* TODO: put a new process to queue [q] */
+    if (q->size == MAX_SIZE_QUEUE || q == NULL)
+    {
+        return;
+    }
+    q->proc[q->size] = proc;
 }
 struct pcb_t *dequeue(struct queue_t *q)
 {
